9212bc679f6b9087ae8a756fd6e2454a16cad22c
Navin Moorthy <navin007.a@gmail.com>
2024-05-15 13:56:49 +0000

    Initial commit

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex-1">
+      <div className="w-full h-full aspect-video flex justify-center items-center">
+        About
+      </div>
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex-1">
+      <div className="w-full h-full aspect-video flex justify-center items-center">
+        Home
+      </div>
+    </main>
+  );
+}
